ce4ded58e68aaefe768fa60c105aa594b3d4c20d
ppaidi <ppaidi@tibco.com>
2020-08-12 20:07:39 +0530

    alter code lint to test lint check job in travis

--- apps/client/src/flogo/app/app-routing.module.ts
+++ apps/client/src/flogo/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FlogoApplicationComponent } from './app.component';
 
-const appRoutes: Routes = [
+let appRoutes: Routes = [
   {
     path: ':appId',
     component: FlogoApplicationComponent,
