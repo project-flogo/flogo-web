7ad6b9f86b4e752057b8031c8fe6ed8b5857a9c0
ppaidi <ppaidi@tibco.com>
2020-10-14 22:46:45 +0530

    test: landing page e2e tests
    - check welcome message
    - check create and delete app functionality

---
+++ apps/client-e2e/src/integration/home.spec.ts
@@ -0,0 +1,62 @@
+describe('flogo web landing page', () => {
+  before(() => {
+    cy.visit('/');
+    cy.get('[data-testid=flogo-spinner]').should('not.be.visible');
+  });
+
+  context('checks the greet message', () => {
+    it('should display welcome message', () => {
+      cy.get('[data-testid=welcome-message]').contains('Welcome back!');
+    });
+  });
+
+  context('checks functionality that creates and that deletes an app', () => {
+    beforeEach(() => {
+      cy.get('[data-testid=apps-list-new]').click();
+      cy.get('[data-testid=flogo-spinner]').should('not.be.visible');
+      cy.get('[data-testid=app-detail-app-name]')
+        .clear()
+        .type(
+          Math.random()
+            .toString(36)
+            .slice(2)
+        );
+      cy.get('[data-testid=app-detail-go-back]').click();
+      cy.get('[data-testid=flogo-spinner]').should('not.be.visible');
+      cy.get('[data-testid=app-list-app]')
+        .first()
+        .trigger('mouseover')
+        .within(() => {
+          cy.get('[data-testid=flogo-spinner]').should('not.be.visible');
+          cy.get('[data-testid=apps-list-app-name]')
+            .invoke('text')
+            .as('appName');
+          cy.get('[data-testid=apps-list-delete-icon]')
+            .should('be.hidden')
+            .click({ force: true });
+          cy.get('[data-testid=flogo-spinner]').should('not.be.visible');
+          cy.get('[data-testid=apps-list-delete-popover]').should('be.visible');
+        })
+        .as('deleteAppModal');
+    });
+
+    it('should not delete the application when cancel button is clicked', () => {
+      cy.get('@deleteAppModal').within(() => {
+        cy.get('[data-testid=apps-list-delete-popover-cancel]').click();
+      });
+      cy.get<string>('@appName').then(appName => {
+        cy.contains(appName);
+      });
+    });
+
+    it('should delete the application when delete button is clicked', () => {
+      cy.get('@deleteAppModal').within(() => {
+        cy.get('[data-testid=apps-list-delete-popover-delete]').click();
+      });
+      cy.get('[data-testid=flogo-spinner]').should('not.be.visible');
+      cy.get<string>('@appName').then(appName => {
+        cy.contains(appName).should('not.exist');
+      });
+    });
+  });
+});
