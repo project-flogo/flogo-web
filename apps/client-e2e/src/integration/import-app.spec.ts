aa4256a5a46e70d8b441593b66de059731d802d1
ppaidi <ppaidi@tibco.com>
2020-10-31 00:21:11 +0530

    test: import legacy application

--- apps/client-e2e/src/integration/import-app.spec.ts
+++ apps/client-e2e/src/integration/import-app.spec.ts
@@ -3,6 +3,9 @@ import { visitApp } from '../utils';
 describe('Import app json', () => {
   before(() => {
     visitApp();
+  });
+
+  beforeEach(() => {
     cy.server();
     cy.route({
       method: 'POST',
@@ -10,15 +13,24 @@ describe('Import app json', () => {
     }).as('importApp');
   });
 
-  it('should be able to import flogo application json', () => {
-    const filePath = 'flogo-app.json';
-    cy.get('[data-cy=import-app-input]').attachFile(filePath, {
-      force: true,
-    });
-    cy.wait('@importApp').then(() => {
-      cy.get('[data-cy=flogo-notification]').contains(
-        'Application imported successfully'
-      );
-    });
+  it('should be able to import legacy V0.5.8 flogo application', () => {
+    const filePath = 'flogo-app - v0.5.8.json';
+    importApp(filePath);
+  });
+
+  it('should be able to import standard V0.9.0 flogo application', () => {
+    const filePath = 'flogo-app - v0.9.0.json';
+    importApp(filePath);
   });
 });
+
+function importApp(filePath) {
+  cy.get('[data-cy=import-app-input]').attachFile(filePath, {
+    force: true,
+  });
+  cy.wait('@importApp').then(() => {
+    cy.get('[data-cy=flogo-notification]').contains('Application imported successfully');
+    // closing notification manually to ensure only one success notification is seen at a time
+    cy.get('[data-cy=flogo-notification-close]').click();
+  });
+}
